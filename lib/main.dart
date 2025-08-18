cc611e464935ffbd43c75fb0a2dc01ef95aa6ac6
Ahmed Ghaith <ahmed.ghaith@cmaster-eg.com>
2025-08-18 10:23:22 +0300

    Remove OneSignal dependency from pubspec.yaml and pubspec.lock, and clean up related initialization code in main.dart to streamline notification handling.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:onesignal_flutter/onesignal_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:khushoo3/view_model/network/bloc_observer.dart';
@@ -14,11 +13,7 @@ void main() async {
   // Initialize Firebase
   await Firebase.initializeApp();
  
-  // Initialize OneSignal
-  OneSignal.initialize("784dab78-8048-44be-9d82-6831b4dcdcd8");
-  
-  // Request notification permission
-  OneSignal.Notifications.requestPermission(true);
+
   
   runApp(HomePage());
 }
